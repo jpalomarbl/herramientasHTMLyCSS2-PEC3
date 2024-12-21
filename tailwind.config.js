e68859b69b9222831b640e60343b9934aff33785
Jaime Simeón Palomar Blumenthal <jpalomarbl@uoc.edu.es>
2024-12-21 17:52:20 +0000

    Finished styles for header mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    fontSize: {
+      'xs': '.8rem',
+       sm: '.9rem',
+       base: '1.2rem',
+       xl: '1.4rem',
+      '2xl': '1.6rem',
+      '3xl': '2rem',
+    },
+    extend: {
+      colors: {
+        'primary': '#297fca',
+        'secondary': '#3fa2f7',
+        'info': '#fff',
+        'light': '#73edff',
+        'dark': '#000078'
+      },
+      fontFamily: {
+        roboto: ['"Roboto"', "serif"],
+        doto: ['"Doto"', "serif"]
+      }
+    },
   },
   plugins: [],
 }
